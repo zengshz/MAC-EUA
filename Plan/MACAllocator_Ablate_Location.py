6465c585789ea67b5b7a71df82abbaba7a917b73
zengshz <161817286+zengshz@users.noreply.github.com>
2026-02-23 14:13:16 +0800

    Update MACAllocator_Ablate_Location.py

--- Plan/MACAllocator_Ablate_Location.py
+++ Plan/MACAllocator_Ablate_Location.py
@@ -383,4 +383,4 @@ class MACAllocator_Ablate_Location(nn.Module):
         # print(servers[:, :, 3:7].sum())
         # print(alloc_num)
 
-        return -reward, logp_accum, alloc_num, alloc_ratio, active_ratio, capacity_used_ratio, p_lat_normalized
+        return -reward, logp_accum, alloc_num, alloc_ratio, active_ratio, capacity_used_ratio, p_lat
